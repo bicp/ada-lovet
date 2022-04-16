3c96e7fde40a57e282115124b54089ec416b6036
Bianca Pimenta <bi.cp1802@gmail.com>
2022-04-16 11:55:09 +0200

    first commit

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Login.css";
+
+export function Login(props) {
+    return <div id="login">
+        <div id="login-form"></div>
+        <div id="login-img"></div>
+    </div>
+}
